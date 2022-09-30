e4b960fd3b9e8f78f0cd96058c7838dc772920c9
Sachin Mathur <samathur175@gmail.com>
2022-09-30 15:49:34 +0530

    Seventh Commit

--- Index.js
+++ Index.js
@@ -1,42 +1,20 @@
-const navslide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+const header = document.getElementById("header");
+const banner = document.querySelector(".main-banner");
 
-  burger.addEventListener("click", () => {
-    // Toggle Nav
-    nav.classList.toggle("nav-active");
-    //Lins Animation
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navlinkFade 1s ease forwards ${
-          index / 7 + 0.5
-        }`;
-      }
-    });
-    // Burger icon annimation
-    burger.classList.toggle("toggle");
+const sectionOneoptions = {};
 
-    //Link seperator icons horizontal
+const sectionOneobserver = new IntersectionObserver(function (
+  entries,
+  sectionOneobserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scroll");
+    } else {
+      header.classList.remove("nav-scroll");
+    }
   });
-};
-navslide();
+},
+sectionOneoptions);
 
-const liTagSap = () => {
-  const burger = document.querySelector(".burger");
-  const tagsap1 = document.getElementById("li-tag-seperator1");
-  const tagsap2 = document.getElementById("li-tag-seperator2");
-
-  burger.addEventListener("click", () => {
-    tagsap1.classList.remove("li-tag-seperator");
-    tagsap2.classList.remove("li-tag-seperator");
-  });
-};
-liTagSap();
-
-// window.addEventListener("scroll", () => {
-//   const header = document.querySelector("header");
-//   header.classList.toggle("nav-scorll", window.scrollY > 0);
-// });
+sectionOneobserver.observe(banner);
